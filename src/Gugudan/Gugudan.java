5b96938d8dd34459d1b2f3389a159b295843b2e4
pjd01 <pjd01@192.168.200.167>
2018-11-26 23:37:35 +0900

    method 구구단

--- src/Gugudan/Gugudan.java
+++ src/Gugudan/Gugudan.java
@@ -2,17 +2,26 @@ package Gugudan;
 import java.util.Scanner;
 
 public class Gugudan {
+	
+	public static int[] calculate(int times){
+		int[] result = new int[9];
+		for(int i = 0; i < result.length; i++) {
+				result[i] = times * (i + 1);
+
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
 
 	public static void main(String[] args) {
-		int[] result = new int[9];
-		for(int j = 2; j <= 9; j++) {
-			for(int i = 0; i < result.length; i++) {
-				result[i] = j * (i + 1);
-			}
-			
-			for(int i = 0; i < result.length; i++) {
-				System.out.println(result[i]);
-				}
-		}	
+		for(int i = 2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
+		}
 	}
 }
