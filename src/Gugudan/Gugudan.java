84dbc95acde537b8837c515230c17c4dfa55ee8d
pjd01 <pjd01@192.168.200.167>
2018-11-26 23:08:57 +0900

    for문 활용한 구구단

--- src/Gugudan/Gugudan.java
+++ src/Gugudan/Gugudan.java
@@ -4,68 +4,15 @@ import java.util.Scanner;
 public class Gugudan {
 
 	public static void main(String[] args) {
-//		8,9dan
-		System.out.println("구구단 중 출력할 단은? ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("number : " +number);
-		
-		if (number < 2) {
-			System.out.println("2 보다 큰수를 입력하시오");
-			
-		}
-
-		else if (number > 9) {
-			System.out.println("9 보다 작은수를 입력하시오");
-			
-		}
-		
-		else {
-			for(int i = 1 ; i < 10; i++) {
-				System.out.println(number * i);
+		int[] result = new int[9];
+		for(int j = 2; j <= 9; j++) {
+			for(int i = 0; i < result.length; i++) {
+				result[i] = j * (i + 1);
 			}
-		}
-		
-		
-//		//6dan
-//		int i = 1;
-//		while(i < 10) {
-//			System.out.println(6 * i);
-//			i++;
-//			
-//		}
-//		
-//		//7dan
-//		for(int j = 1; j < 10 ; j++) {
-//			System.out.println(7 * j);
-//		}
-//		
-//		
-//		//2단 
-//		System.out.println("2 * 1");
-//		System.out.println("2 * 2");
-//		System.out.println("2 * 3");
-//		System.out.println("2 * 4");
-//		System.out.println("2 * 5");
-//		System.out.println("2 * 6");
-//		System.out.println("2 * 7");
-//		System.out.println("2 * 8");
-//		System.out.println("2 * 9");
-//		
-//		//3단
-//		System.out.println("3 * 1");
-//		System.out.println("3 * 2");
-//		System.out.println("3 * 3");
-//		System.out.println("3 * 4");
-//		System.out.println("3 * 5");
-//		System.out.println("3 * 6");
-//		System.out.println("3 * 7");
-//		System.out.println("3 * 8");
-//		System.out.println("3 * 9");
-//		
-//		//4dan
-//		
-//		
+			
+			for(int i = 0; i < result.length; i++) {
+				System.out.println(result[i]);
+				}
+		}	
 	}
-
 }
